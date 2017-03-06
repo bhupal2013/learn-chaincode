beccbef5997ff7b8706e6efded51060267112518
bhupal2013 <Ramabhupal.Thumu@cognizant.com>
2017-03-06 20:16:57 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -160,15 +160,15 @@ var err error
 // "1x22" "supplier" 20 
 // args[0] args[1] args[2] 
 
-uuid, err := newUUID()
+Uuid, err := newUUID()
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
-	fmt.Printf("%s\n", uuid)
+	fmt.Printf("%s\n", Uuid)
 user := args[0]
 litres :=args[1] 
 	
-	id := uuid
+	id := Uuid
 // Checking if the container already exists in the network
 milkAsBytes, err := stub.GetState(id) 
 if err != nil {
@@ -191,7 +191,7 @@ res.ContainerID = id
 res.User = user
 res.Litres = litres
 milkAsBytes, _ =json.Marshal(res)
-
+	fmt.Printf("%v+\n",res)
 stub.PutState(res.ContainerID,milkAsBytes)
 	
 //Update containerIndexStr	
@@ -227,16 +227,16 @@ stub.PutState(res.ContainerID,milkAsBytes)
 }
 
 func newUUID() (string, error) {
-	uuid := make([]byte, 16)
-	n, err := io.ReadFull(rand.Reader, uuid)
-	if n != len(uuid) || err != nil {
+	Uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, Uuid)
+	if n != len(Uuid) || err != nil {
 		return "", err
 	}
 	// variant bits; see section 4.1.1
-	uuid[8] = uuid[8]&^0xc0 | 0x80
+	Uuid[8] = Uuid[8]&^0xc0 | 0x80
 	// version 4 (pseudo-random); see section 4.1.3
-	uuid[6] = uuid[6]&^0xf0 | 0x40
-	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+	Uuid[6] = Uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", Uuid[0:4], Uuid[4:6], Uuid[6:8], Uuid[8:10], Uuid[10:]), nil
 }
 
 /******************Asset Coin creation****************/
